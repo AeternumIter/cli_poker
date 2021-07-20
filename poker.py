05f8aadeb72403146cdff0d0d8333af1936d6c37
Jacob Silcoff <jacobsilcoff@gmail.com>
2021-07-20 13:42:34 -0400

    Refactored player data into game, fixed bugs

--- poker.py
+++ poker.py
@@ -1,6 +1,4 @@
 import os
-from dataclasses import dataclass
-
 
 # Constants -- will likely configure based on cli inputs
 starting_amt = 500
@@ -9,55 +7,96 @@ sb = 5
 bb = 10
 
 
-@dataclass
-class Player:
-    name: str
-    money: int = starting_amt
-    def __str__(self):
-        return self.name
-    def __repr__(self):
-        return f"{self.name} [{self.money}]"
-    def __hash__(self):
-        return self.name.__hash__()
-
-@dataclass
 class Game:
-    players: list[Player]
+    players: list[str]
+    player_money: dict[str, int] = {}
     curr_round: int = 0
     dealer_index: int = 0
 
     # variables associated w/ betting
     requirement: int = 0
-    investments: dict[Player, int] = {}
-    players_in : set[Player] = None
-    side_pots: set[(Player, amt)] = {}
+    investments: dict[str, int] = {}
+    players_in: set[str] = None
+
+    dealer: int = 0
+    small_blind: int = 1
+    big_blind: int
+
+    def __init__(self, players):
+        self.players = [player.lower() for player in players]
+        self.player_money = {player: starting_amt for player in players}
+        self.dealer = 0
+        self.small_blind = 1
+        self.big_blind = 2 % len(self.players)
 
     def start_round(self):
         self.requirement = 0
         self.investments = {}
-        self.players_in = set(players)
-        self.side_pots = {}
-        # TODO filter players that need to be removed
+        self.players_in = set(self.players)
+        self.curr_round += 1
 
-    def place_bet(self, player : Player, amt : int):
+        clear()
+        print("--------------------------")
+        print(f"Round {self.curr_round}")
+        print(f"Small blind: {self.players[self.small_blind]}")
+        print(f"Big blind: {self.players[self.big_blind]}")
+        print("--------------------------")
+
+    def pass_button(self):
+        self.dealer = (self.dealer + 1) % len(self.players)
+        self.small_blind = (self.dealer + 1) % len(self.players)
+        self.big_blind = (self.dealer + 2) % len(self.players)
+
+    def players_that_can_bet(self):
+        return filter(lambda x: self.player_money[x] > 0, self.players_in)
+
+    def place_bet(self, player: str, amt: int):
         if amt < 0:
             # fold by betting -1
             self.players_in.remove(player)
             return
         player.money -= amt
         self.investments[player] = self.investments.get(player, 0) + amt
-        if self.investments[player] < requirement:
-            side_pots.add((player, self.investments[player]))
 
-    def payout():
-        # TODO deal w/ payout logic -- promting for rankings happens here
+    def payout(self):
+        # TODO: deal with ties [shouldnt be hard]
+        prompt = "Who won overall?"
+        while sum(self.investments.values()) > 0:
+            # gets the curr best player still in
+            print(prompt)
+            ans = input().lower()
+            while ans not in self.players_in:
+                print(f"Please enter one of the following: [{', '.join(self.players_in)}]")
+                ans = input().lower()
 
-        """
-        basic idea -- ask for winners. If the winners are in a side pot, distribute said pot and all lower valued pots to the winners, and remove these players -- then ask for the runner up etc [etc does a lot of work lol]
+            # if winner not in a side pot, we are done in one step
+            if self.investments[ans] < self.requirement:
+                self.player_money[ans] += sum(self.investments.values())
+                break
 
-        """
+            for player, amt in self.investments.items().copy():
+                if player == ans:
+                    continue
+                winnings = min(amt, self.investment[ans])
+                self.player_money[ans] += winnings
+                self.investment[player] -= winnings
 
-        pass
+            # always get ur $ back when win"
+            self.player_money[ans] += self.investment[player]
+            self.investment[ans] = 0
+            self.players_in.remove(ans)
+            prompt = f"After {ans}, who won?"
+
+        # print resulting scores
+        print("RESULTS: ")
+        to_remove = set()
+        for player, amt in self.player_money.items():
+            print(f"{player} has ${amt}{'' if not amt else ' [eliminated]'}")
+            if amt == 0:
+                to_remove.add(player)
+        for player in to_remove:
+            del self.player_money[player]
+            self.players.remove(player)
 
 
 def clear():
@@ -66,10 +105,11 @@ def clear():
     except:
         print('\n\n\n')
 
+
 def get_bet(game, player):
     print(f"{player}, please enter your bet (enter '(c)all'/'(c)heck', '(f)old', or raise amount):")
     while True:
-        ans = input().lower()
+        ans = input().lower().strip()
         if ans == "fold" or ans == "f":
             return -1
         if ans == "check" or ans == "call" or ans == "c":
@@ -78,39 +118,22 @@ def get_bet(game, player):
             ans = int(ans)
             if (ans < 0):
                 print("You cannot raise by a negative amount")
-            elif ans <= player.money:
+            elif ans <= game.player_money[player]:
                 return ans
             else:
-                print(f"You only have ${player.money} to bet.")
+                print(f"You only have ${game.player_money[player]} to bet.")
         except:
             print("You must enter a number or valid move (c/f)")
-
 
 
 def __main__():
     num_players = int(input("How many players? "))
     print("Enter players in order")
-    game = Game([Player(input()) for _ in range(num_players)])
+    game = Game([input() for _ in range(num_players)])
 
     # main game loop
     while len(game.players) > 1:
-        # sets up round
-        small_blind = (game.dealer_index + 1) % num_players
-        big_blind = (game.dealer_index + 2) % num_players
-
-        clear()
-        print("--------------------------")
-        print(f"Round {game.curr_round}")
-        print(f"Small blind: {game.players[small_blind]}")
-        print(f"Big blind: {game.players[big_blind]}")
-        print("--------------------------")
-
-
-        still_betting = set(game.players)
-
-
-        #TODO: put logic for side pots somewhere
-
+        game.start_round()
         # handle ante & blinds (TODO)
 
         # go through 4 stages of betting
@@ -119,11 +142,10 @@ def __main__():
 
             # go through each player to get bets
             # TODO: ensure propper ordering relative to dealer
-            for p in game.players:
+            for p in game.players_that_can_bet():
                 bet = get_bet(game, p)
-                if not still_betting: pass
-        game.dealer_index = small_blind
-        game.curr_round += 1
-    print(f"The winner is {next(iter(players))}.")
+
+    print(f"The winner is {next(iter(game.players))}.")
+
 
 __main__()
